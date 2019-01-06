e3c25da325798c37eebba82ab3a17713bc5ec5b8
pauli <pauli@10.3.1.125>
2019-01-06 21:54:06 +0100

    Ajout modifs Daouya + mise en place du type Date

--- src/myutil/Constantes.java
+++ src/myutil/Constantes.java
@@ -15,9 +15,10 @@ public class Constantes {
     public static final String URL = "jdbc:derby://localhost:1527/LSI_L3_JAVA";
     public static final String USER = "adm";
     public static final String MDP = "adm";
-    public static final String REQUETE_TOUS = "SELECT * FROM PROGRAMMEUR";
-    public static final String REQUETE_UNIQUE = "SELECT * FROM PROGRAMMEUR WHERE MATRICULE = ?";
+    public static final String SELECT_ALL = "SELECT * FROM PROGRAMMEUR";
+    public static final String SELECT_UNIQUE = "SELECT * FROM PROGRAMMEUR WHERE MATRICULE = ?";
     public static final String UPDATE_UNIQUE = "UPDATE PROGRAMMEUR SET NOM = ?, PRENOM = ?, ADRESSE = ?, PSEUDO = ?, RESPOONSABLE = ?, HOBBY = ?, DATE_NAISS = ?, DATE_EMB = ? WHERE MATRICULE = ?";
     public static final String DELETE_UNIQUE = "DELETE * FROM PROGRAMMEUR WHERE MATRICULE = ?";
+    public static final String INSERT_UNIQUE = "INSERT INTO PROGRAMMEUR (MATRICULE, NOM, PRENOM, ADRESSE, PSEUDO, RESPOONSABLE, HOBBY, DATE_NAISS, DATE_EMB) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
 }
